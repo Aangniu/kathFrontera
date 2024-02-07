948d4be36b5f29164f1683f8b2f4c0bf7afe6a1c
aangniu <aangniu96@gmail.com>
2024-02-08 00:16:14 +0100

    update also client

--- client/client.py
+++ client/client.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         parser = argparse.ArgumentParser()
         parser.add_argument("port", help="port", type=int)
         args = parser.parse_args()
-        
+
         address = f"http://localhost:{args.port}"
         server_available = False
         while not server_available:
@@ -19,13 +19,13 @@ if __name__ == "__main__":
                         print("Server not available")
                         time.sleep(10)
 
-        def eval_um_model(traction_middle):
-                config = {"order": 6}
-                return model([[78e6, traction_middle * 1e6, 62e6]], config)
+        def eval_um_model(Cd):
+                config = {"meshFile": "model_0p1Hz"}
+                return model([[500, Cd, 28518750000, 24637500000]], config)
 
-        start_time = time.time() 
+        start_time = time.time()
         with mp.Pool(10) as p:
-                result = p.map(eval_um_model, [81.2, 81.3, 81.4, 81.5, 81.6, 81.7, 81.8, 81.9, 82.0, 82.1])
+                result = p.map(eval_um_model, [5,10,20])
                 print(result)
         end_time = time.time()
         print(end_time - start_time)
